d51f1310cf2c09de5a12c70171ca88c8b782880a
Mantu Kumar Ojha <mk420464@gmail.com>
2023-09-23 03:53:57 +0530

    Added related fragments and drawables

---
+++ app/src/main/java/com/fridayhouse/snoozz/ui/fragments/ComposeFragment.kt
@@ -0,0 +1,26 @@
+package com.fridayhouse.snoozz.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.fridayhouse.snoozz.R
+
+class ComposeFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_compose, container, false)
+    }
+
+}
